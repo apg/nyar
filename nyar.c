5f14e6b882ebb4d30a39e85fa988b38d69995f4f
apgwoz <git@apgwoz.com>
2011-12-19 08:53:08 -0500

    * almost there for a working first version

--- nyar.c
+++ nyar.c
@@ -1,75 +1,25 @@
 #include <sys/ioctl.h>
+#include <sys/types.h>
+#include <poll.h>
 #include <signal.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-#define BLACK 30
-#define RED 31
-#define GREEN 32
-#define YELLOW 33
-#define BLUE 34
-#define MAGENTA 35
-#define CYAN 36
-#define WHITE 37
+#define MIN(x, y) (x > y ? y: x)
 
-
-// current width of the terminal
 static int current_width;
+static int needs_relaunch = 0;
 
-// possible foreground colors
-static int foreground_colors[] = {
-  RED, YELLOW, GREEN, BLUE, MAGENTA, CYAN, GREEN
+#define NEXT_THROBBER (_throbber[(_throbber_i++) % 4])
+static char _throbber[] = {
+  '/', '-', '\\', '|'
 };
-static int _foreground_color_i = 0;
+static int _throbber_i = 0;
 
-#define NEXT_FOREGROUND (_foreground_color_i++ % 7)
-
-// possible background colors
-static int background_colors[] = {
-  RED+10, YELLOW+10, GREEN+10, BLUE+10, MAGENTA+10, CYAN+10, GREEN+10
-};
-static int _background_color_i = 0;
-
-#define NEXT_BACKGROUND (_background_color_i++ % 7)
-
-// tail make up
-static char *tail_bits[2] = {
-  "v", "~", "^", "~"
-};
-static int _tail_bit_i = 0;
-
-#define NEXT_TAILBIT (_tail_bit_i++ % 4)
-
-// leg shift
-static int leg_shift[] = {
-  -1, 0, 1
-};
-
-static int _leg_shift_i = 0;
-
-#define NEXT_LEGSHIFT (_leg_shift_i++ % 3)
-
-// toast position
-static int toast[] = {
-  1, 2
-};
-
-static int _toast_i = 0;
-
-#define NEXT_TOAST (_toast_i++ % 2)
-
-static char *stream[3] = {
-  "~_", "_-"
-};
-
-#define NEXT_STREAM (_stream_i++ % 2)
-
-#define CURSOR_UP_TMPL "\x1b[%dA"
-#define FOREGROUND_TMPL "\x1b[1;%dm"
-#define BACKGROUNDED_TMPL "\x1b[1;%d;%dm"
-#define RESET "\x1b[0;m"
 
 static int
 term_width() 
@@ -86,22 +36,11 @@ signal_handler(int signum)
   case SIGWINCH:
     current_width = term_width();
     break;
+  case SIGCHLD:
+    printf("sigchld\n");
+    needs_relaunch = 1;
+    break;
   }
-}
-
-// wrap str in color, then reset the colors
-static void
-colored(int fg, int bg, char *str, char *buf, size_t bufsiz)
-{
-  char tmp[128];
-  if (bg > 0) {
-    snprintf(tmp, 128, BACKGROUNDED_TMPL, bg, fg);
-  }
-  else {
-    snprintf(tmp, 128, FOREGROUND_TMPL, fg);
-  }
-  
-  sprintf(buf, bufsiz, "%s%s%s", tmp, str, RESET);
 }
 
 static void
@@ -116,32 +55,127 @@ fill(char *dest, char *src, size_t maxsize, size_t srcsiz)
   // 1 in the case that these are not evenly divisible.
 }
 
-
 static void
-draw_one(int amt)
+draw_one(int amt, int max)
+{
+  int i;
+  char fmt[32];
+  char buf[1024];
+  float percentage = amt / (float)max;
+  int width = MIN((int)((current_width - 11) * percentage), 1024);
+  
+
+  fill(buf, "=", width, 1);
+  buf[width] = 0;
+
+  memset(fmt, 0, 32);
+
+  // create format string which takes into consideration width
+  snprintf(fmt, 32, "[%%-%ds] %%c %%-4.1f%%%%\n", (current_width - 11));
+  printf(fmt, buf, NEXT_THROBBER, 100 * percentage);
+}
+
+static int last_number = 0;
+
+static int
+read_last_number(int fd)
 {
   char buf[1024];
-  char tmp[1024];
-  int background = NEXT_BACKGROUND;
-  int foreground = NEXT_FOREGROUND;
-  char *st = NEXT_STREAM;
-  char tailbit = NEXT_TAILBIT;
-  int legshift = NEXT_LEGSHIFT;
-  int toastpos = NEXT_TOAST;
+  int br;
+  int i;
+  br = read(fd, buf, 1024);
+  // TODO: this is broken severely
+  if (br > 0) {
+    buf[br-1] = 0;
+    return atoi(buf);
+  }
+  return 0;
+}
 
-  // STUB
+static void
+main_loop(int finish, int argc, char *argv[])
+{
+  int i = 0; // tmp
+  char buf[10];
+  int pdes[2] = {-1, -1};
+  pid_t chld;
+  int progress = 0;
+  int completed = 0;
+  int ps;
+  struct pollfd pfd;
 
-  //#define CAT_1_TMPL "\x1b[2K%s,------,"
-  //#define CAT_2_TMPL "\x1b[2K%s%s| %s /\\_/\\"
-  //#define CAT_3_TMPL "\x1b[2K%s|_%s( ^ w^)"
-  //#define CAT_4_TMPL "\x1b[2K%s    \"  \""
+ loop:
+  needs_relaunch = 0;
+
+  if (pipe(pdes) < 0) {
+    perror("pipe");
+    _exit(EXIT_FAILURE);
+  }
+
+  chld = fork();
+  if (chld < 0) {
+    // FAIL
+    perror("fork");
+    _exit(EXIT_FAILURE);
+  }
+  else if (chld == 0) {
+    close(pdes[0]); // close read
+
+    if (pdes[1] > STDOUT_FILENO) {
+      if (dup2(pdes[1], STDOUT_FILENO) < 0) {
+        perror("dup2-child");
+        _exit(EXIT_FAILURE);
+      }
+      close(pdes[1]);
+    }
+
+    // EXEC Should happen here with /bin/sh -c {whatever}
+    execl("/bin/sh", "-c", "/bin/echo", "4");
+  }
+  else {
+    close(pdes[1]); // close pipe write end
+
+    if (pdes[0] > 0) {
+      if (dup2(pdes[0], STDIN_FILENO) < 0) { // dup2 to stdin
+        perror("dup2");
+        _exit(EXIT_FAILURE);
+      }
+      close(pdes[0]);
+    }
+
+    pfd.fd = STDIN_FILENO;
+    pfd.events = POLLIN;
+
+
+    completed = (progress / finish) >= 1;
+    // poll here on STDIN_FILENO and update after each timeout
+    while (!needs_relaunch && !completed) {
+      ps = poll(&pfd, 1, 200);
+      if (ps < 0) {
+        perror("perror");        
+        _exit(EXIT_FAILURE);
+      }
+      else if (ps > 0) {
+        progress = read_last_number(STDIN_FILENO);
+      }
+      draw_one(progress, finish);
+      completed = (progress / finish) >= 1;
+    }
+
+    if (!completed && needs_relaunch) {
+      sleep(1);
+      goto loop;
+    }
+    kill(chld, SIGTERM);
+    waitpid(&chld);
+  }
 }
 
 int
 main(int argc, char **argv)
 {
   struct sigaction saction;
-  int again_80 = -1;
+  int i = 0; 
 
   sigemptyset(&saction.sa_mask);
   saction.sa_flags = 0;
@@ -149,23 +183,23 @@ main(int argc, char **argv)
 
   if (sigaction(SIGWINCH, &saction, NULL)) {
     perror("sigaction");
-    exit(1);
+    _exit(EXIT_FAILURE);
+  }
+  if (sigaction(SIGCHLD, &saction, NULL)) {
+    perror("sigaction");
+    _exit(EXIT_FAILURE);
   }
 
   current_width = term_width();
 
-  while (again_80 < 1) {
-    if (current_width != 80) {
-      again_80 = 0;
-      printf("Current width: %d\n", current_width);
-      sleep(1);
-    }
-    else if (current_width == 80 && again_80 > 0) {
-      break;
-    }
-    else if (current_width == 80 && !again_80) {
-      again_80 = 1;
-    }
+  if (argc > 2) {
+    main_loop(100, argc - 2, &argv[2]);
   }
+  else {
+    printf("usage: %s <max val> <cmd> <cmd arg 1> <cmd arg ...> <cmd arg N>\n", 
+           argv[0]);
+    _exit(EXIT_FAILURE);
+  }
+
   return 0;
 }
